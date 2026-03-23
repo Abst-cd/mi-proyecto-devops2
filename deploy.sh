f5382a5467cc77f072774a2f16c1e45a8899f8ab
Abst-cd <pacodejesus2006@gmail.com>
2026-03-23 16:34:12 -0700

    update

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Sincronizando archivos con S3"
-aws s3 sync src/ s3://mi-bucket-devops --delete --acl public-read
+aws s3 sync src/ s3://bucketdevops2333 --delete
 echo "Despliegue completado"
 
